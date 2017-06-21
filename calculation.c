75bb28bbb6256712608e4d81ae542bc8cd4d6f86
Daniiloooo <dan98l@mail.ru>
2017-06-21 18:58:25 +0700

    Creating functions

---
+++ calculation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int addition(int S, int l)
+{
+    int summ = 0;
+    summ = S + l;
+    
+    return summ;
+}
+
+int subtraction(int S, int l)
+{
+    int minm = 0;
+    minm = S - l;
+    
+    return minm;
+}
+
+int multiplication(int S, int l)
+{
+    int pro = 0;
+    pro = S * l;
+    
+    return pro;
+}
---
+++ function.h
@@ -0,0 +1,3 @@
+int addition(int S,int l);
+int subtraction(int S,int l);
+int multiplication(int S,int l);
