a0197e66851930bd4a7531544202232185abb7c6
Daniiloooo <dan98l@mail.ru>
2017-06-21 21:57:34 +0700

    Adding output to the console

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
 #include "function.h"
+
+double condition(double operand);
+
+double oneNumber = 0;
+double twoNumber = 0;
+double operand = 0;
 
 int main(){
     
-    double oneNumber = 0;
-    double twoNumber = 0;
-    int operand = 0;
+    double sum = 0;
     
     printf("Добро пожаловать в программу калькулятор! \n");
     printf("В данной программе:'1 соответсвует +' \n");
@@ -17,11 +20,27 @@ int main(){
     printf("Введите первое число: \n");
     scanf("%lf", &oneNumber);
     printf("Введите операцию : \n");
-    scanf("%d", &operand);
+    scanf("%lf", &operand);
     printf("Введите второе число: \n");
     scanf("%lf", &twoNumber);
-
-   
+    
+    sum = condition(operand);
+    printf("Ответ: ");
+    printf("%lf", sum);
+    printf("\n");
+    
     return 0;
     
+}
+
+double condition(double operand){
+    if (operand == 1) {
+        return addition(oneNumber,twoNumber);
+    }else if (operand == 2) {
+        return subtraction(oneNumber,twoNumber);
+    }else if (operand == 3) {
+        return multiplication(oneNumber,twoNumber);
+    }else if (operand == 4) {
+        return division(oneNumber,twoNumber);
+    }else return 0;
 }
